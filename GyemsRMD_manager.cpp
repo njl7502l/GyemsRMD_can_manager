ce11d9aedf5bff3006abd694f4ea4ea8b786f666
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-15 16:32:16 +0900

    add file

---
+++ GyemsRMD_manager.cpp
@@ -0,0 +1,1 @@
+#include <GyemsRMD_manager.h>
---
+++ GyemsRMD_manager.h
@@ -0,0 +1,14 @@
+#ifndef GyemsRMD_manager_h
+#define GyemsRMD_manager_h
+
+#include <Arduino.h>
+#include <FlexCAN_T4_manager.h>
+
+class GyemsRMD_manager {
+private:
+
+public:
+
+};
+
+#endif
