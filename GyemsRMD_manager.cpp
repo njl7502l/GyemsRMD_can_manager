486633d46e51be6347cf01e009750ea3740a545d
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-16 09:33:57 +0900

    とりあえずコミット

--- GyemsRMD_manager.cpp
+++ GyemsRMD_manager.cpp
@@ -9,26 +9,83 @@ void GyemsRMD_manager::read(FlexCAN_T4_manager *canmanager) {
   case 0x30:
   case 0x31:
   case 0x32:
-    gotData.anglePidKp = data[2];
-    gotData.anglePidKi = data[3];
-    gotData.speedPidKp = data[4];
-    gotData.speedPidKi = data[5];
-    gotData.iqPidKp = data[6];
-    gotData.iqPidKi = data[7];
+    gotData.pid.angleKp = data[2];
+    gotData.pid.angleKi = data[3];
+    gotData.pid.speedKp = data[4];
+    gotData.pid.speedKi = data[5];
+    gotData.pid.iqKp = data[6];
+    gotData.pid.iqKi = data[7];
     break;
   case 0x33:
-    rawAccel.c[0] = data[4];
-    rawAccel.c[1] = data[5];
-    rawAccel.c[2] = data[6];
-    rawAccel.c[3] = data[7];
-    gotData.accel = rawAccel.i;
+    i32tc.i = 0;
+    i32tc.c[0] = data[4];
+    i32tc.c[1] = data[5];
+    i32tc.c[2] = data[6];
+    i32tc.c[3] = data[7];
+    gotData.accel.current = i32tc.i;
     break;
   case 0x34:
-    rawTargetAccel.c[0] = data[4];
-    rawTargetAccel.c[1] = data[5];
-    rawTargetAccel.c[2] = data[6];
-    rawTargetAccel.c[3] = data[7];
-    gotData.targetAccel = rawTargetAccel.i;
+    i32tc.i = 0;
+    i32tc.c[0] = data[4];
+    i32tc.c[1] = data[5];
+    i32tc.c[2] = data[6];
+    i32tc.c[3] = data[7];
+    gotData.accel.target = i32tc.i;
+    break;
+  case 0x90:
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[2];
+    ui16tc.c[1] = data[3];
+    gotData.encoder.current = ui16tc.i;
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[4];
+    ui16tc.c[1] = data[5];
+    gotData.encoder.original = ui16tc.i;
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[6];
+    ui16tc.c[1] = data[7];
+    gotData.encoder.offset = ui16tc.i;
+    break;
+  case 0x91:
+  case 0x19:
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[6];
+    ui16tc.c[1] = data[7];
+    gotData.encoder.offset = ui16tc.i;
+    break;
+  case 0x92:
+    i64tc.i = 0;
+    i64tc.c[0] = data[1];
+    i64tc.c[1] = data[2];
+    i64tc.c[2] = data[3];
+    i64tc.c[3] = data[4];
+    i64tc.c[4] = data[5];
+    i64tc.c[5] = data[6];
+    i64tc.c[6] = data[7];
+    gotData.angle.rawMultiTurn = i64tc.i;
+    gotData.angle.multiTurn = (0.01 * i64tc.i);
+    break;
+  case 0x94:
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[6];
+    ui16tc.c[1] = data[7];
+    gotData.angle.rawSingleTurn = ui16tc.i;
+    gotData.angle.singleTurn = (0.01 * ui16tc.i);
+    break;
+  case 0x9A:
+    gotData.information.temperature = data[1];
+    ui16tc.i = 0;
+    ui16tc.c[0] = data[3];
+    ui16tc.c[1] = data[4];
+    gotData.information.rawVoltage = ui16tc.i;
+    gotData.information.voltage = (0.1 * ui16tc.i);
+    gotData.information.error.rawState = data[7];
+    gotData.information.error.lowVoltageProtection = bitRead(data[7],0);
+    gotData.information.error.overTemperatureProtection = bitRead(data[7],3);
+    break;
+  case 0x9C:
+    gotData.information.temperature = data[1];
+
     break;
   default:
     break;
--- GyemsRMD_manager.h
+++ GyemsRMD_manager.h
@@ -10,26 +10,70 @@ private:
       0,    0x30, 0x31, 0x32, 0x33, 0x34, 0x90, 0x91, 0x19, 0x92, 0x94, 0x9A,
       0x9B, 0x9C, 0x9D, 0x80, 0x81, 0x88, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6};
   
-  union i32toC {
+  union Int16toChar {
+    int16_t i;
+    uint8_t c[2];
+  } i16tc;
+  union Int32toChar {
     int32_t i;
     uint8_t c[4];
-  };
-  union ui16toC {
+  } i32tc;
+  union Int64toChar {
+    int64_t i;
+    uint8_t c[8];
+  } i64tc;
+  union Uint16toChar {
     uint16_t i;
     uint8_t c[2];
-  };
+  } ui16tc;
 
-  i32toC rawAccel;       // unit: 1dps/s
-  i32toC rawTargetAccel; // unit: 1dps/s
-
-  class values {
+  class Values {
   public:
-    uint8_t anglePidKp, anglePidKi;
-    uint8_t speedPidKp, speedPidKi;
-    uint8_t iqPidKp, iqPidKi;
+    class Pid { // 0-255 uint value
+    public:
+      uint8_t angleKp, angleKi;
+      uint8_t speedKp, speedKi;
+      uint8_t iqKp, iqKi;
+    } pid;
 
-    int32_t accel;
-    int32_t targetAccel;
+    class Accel { // unit 1[dps/s]
+    public:
+      int32_t current;
+      int32_t target;
+    } accel;
+
+    class Encoder { // 14bit encoder value range 0-0x3FFF
+    public:
+      uint16_t current;
+      uint16_t original;
+      uint16_t offset;
+    } encoder;
+
+    class Angle {
+    public:
+      int64_t rawMultiTurn; // unit 0.01[deg/LSB]
+      double multiTurn;     // unit [deg]
+
+      uint16_t rawSingleTurn; // unit 0.01[deg/LSB]
+      double singleTurn;      // unit [deg] (unsigned)
+    } angle;
+
+    class Torque{
+      int16_t rawIq;
+    }torque;
+
+    class Information {
+    public:
+      int8_t temperature; // unit 1[c'/LSB]
+      uint16_t rawVoltage;   // unit 0.1[v/LSB]
+      double voltage;
+      class Error{
+        public:
+        uint8_t rawState;
+        bool lowVoltageProtection;
+        bool overTemperatureProtection;
+      }error;
+    } information;
   };
 
   int motorId = 0;
@@ -38,7 +82,7 @@ public:
   void setId(int Id);
   void read(FlexCAN_T4_manager *canmanager);
 
-  values gotData;
+  Values gotData;
 };
 
 #endif
